f22b1bc6f937e740a1776159fcaa616423dd9818
jeison🌱 <l1996021@gmail.com>
2026-04-09 20:27:39 -0500

    chore: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   build: {
     format: 'file', // Disable directory urls (like /about/index.html to just /about.html)
   },
+
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
